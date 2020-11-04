fd3da1214fb5efb04652979b4f5e7bbb09dfaf46
piyush12346789 <piyushgupta024@gmail.com>
2020-11-04 07:41:33 +0530

    [Piyush] Add: Method to find cheapest for a given date range.

---
+++ HotelReservationSystem/Operation.cs
@@ -0,0 +1,155 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HotelReservationSystem
+{
+    public class Operation
+    {
+        private static int totalCost = 0;
+        public static int GetMonthInDigits(string month)
+        {
+            switch (month.ToLower())
+            {
+                case "jan":
+                    return 1;
+                case "feb":
+                    return 2;
+                case "mar":
+                    return 3;
+                case "apr":
+                    return 4;
+                case "may":
+                    return 5;
+                case "jun":
+                    return 6;
+                case "jul":
+                    return 7;
+                case "aug":
+                    return 8;
+                case "sep":
+                    return 9;
+                case "oct":
+                    return 10;
+                case "nov":
+                    return 11;
+                case "dec":
+                    return 12;
+                default:
+                    Console.WriteLine("Invalid Month");
+                    return 0;
+            }
+        }
+        public static string GetDay(string date)
+        {
+            int day = Convert.ToInt32(date.Substring(0, 2));
+            string month = date.Substring(2, 3);
+            int year = Convert.ToInt32(date.Substring(5));
+            DateTime d = new DateTime(year, Operation.GetMonthInDigits(month), day);
+            return d.DayOfWeek.ToString();
+        }
+        public static int GetLakewoodCost(string checkInDate, string checkOutDate)
+        {
+            totalCost = 0;
+            Hotel lakeWood = new Hotel();
+            lakeWood.WeekdayRateForRegularCustomer = 110;
+            lakeWood.WeekendRateForRegularCustomer = 90;
+            string checkInday = GetDay(checkInDate);
+            string checkOutday = GetDay(checkOutDate);
+            if (checkInday.Equals("Saturday") || checkInday.Equals("Sunday"))
+            {
+                totalCost += lakeWood.WeekendRateForRegularCustomer;
+            }
+            else
+            {
+                totalCost += lakeWood.WeekdayRateForRegularCustomer;
+            }
+            if (checkOutday.Equals("Saturday") || checkOutday.Equals("Sunday"))
+            {
+                totalCost += lakeWood.WeekendRateForRegularCustomer;
+            }
+            else
+            {
+                totalCost += lakeWood.WeekdayRateForRegularCustomer;
+            }
+            return totalCost;
+        }
+        public static int GetBridgewoodCost(string checkInDate, string checkOutDate)
+        {
+            totalCost = 0;
+            Hotel bridgeWood = new Hotel();
+            bridgeWood.WeekdayRateForRegularCustomer = 160;
+            bridgeWood.WeekendRateForRegularCustomer = 60;
+            string checkInday = GetDay(checkInDate);
+            string checkOutday = GetDay(checkOutDate);
+            if (checkInday.Equals("Saturday") || checkInday.Equals("Sunday"))
+            {
+                totalCost += bridgeWood.WeekendRateForRegularCustomer;
+            }
+            else
+            {
+                totalCost += bridgeWood.WeekdayRateForRegularCustomer;
+            }
+            if (checkOutday.Equals("Saturday") || checkOutday.Equals("Sunday"))
+            {
+                totalCost += bridgeWood.WeekendRateForRegularCustomer;
+            }
+            else
+            {
+                totalCost += bridgeWood.WeekdayRateForRegularCustomer;
+            }
+            return totalCost;
+        }
+        public static int GetRidgewoodCost(string checkInDate, string checkOutDate)
+        {
+            totalCost = 0;
+            Hotel ridgeWood = new Hotel();
+            ridgeWood.WeekdayRateForRegularCustomer = 220;
+            ridgeWood.WeekendRateForRegularCustomer = 150;
+            string checkInday = GetDay(checkInDate);
+            string checkOutday = GetDay(checkOutDate);
+            if (checkInday.Equals("Saturday") || checkInday.Equals("Sunday"))
+            {
+                totalCost += ridgeWood.WeekendRateForRegularCustomer;
+            }
+            else
+            {
+                totalCost += ridgeWood.WeekdayRateForRegularCustomer;
+            }
+            if (checkOutday.Equals("Saturday") || checkOutday.Equals("Sunday"))
+            {
+                totalCost += ridgeWood.WeekendRateForRegularCustomer;
+            }
+            else
+            {
+                totalCost += ridgeWood.WeekdayRateForRegularCustomer;
+            }
+            return totalCost;
+        }
+        public static int FindCheapestHotelRate(string checkInDate, string checkOutDate)
+        {
+            int lakeWoodCost = GetLakewoodCost(checkInDate, checkOutDate);
+            int bridgeWoodCost = GetBridgewoodCost(checkInDate, checkOutDate);
+            int ridgeWoodCost = GetRidgewoodCost(checkInDate, checkOutDate);
+            int leastCost = lakeWoodCost < bridgeWoodCost ? lakeWoodCost : bridgeWoodCost;
+            leastCost = leastCost < ridgeWoodCost ? leastCost : ridgeWoodCost;
+            return leastCost;
+        }
+        public static string FindCheapestHotelName(string checkInDate, string checkOutDate)
+        {
+            int leastCost = FindCheapestHotelRate(checkInDate, checkOutDate);
+            if (leastCost == GetLakewoodCost(checkInDate, checkOutDate))
+            {
+                return "Lakewood";
+            }
+            else if (leastCost == GetBridgewoodCost(checkInDate, checkOutDate))
+            {
+                return "Bridgewood";
+            }
+            else
+            {
+                return "Ridgewood";
+            }
+        }
+    }
+}
--- HotelReservationUnitTest/UnitTest1.cs
+++ HotelReservationUnitTest/UnitTest1.cs
@@ -7,26 +7,14 @@ namespace HotelReservationUnitTest
     public class UnitTest1
     {
         [TestMethod]
-        public void Adding_HotelName_And_RegularCustomerRates_And_Verifying()
+        public void Given_DateRange_Should_Return_CheapestHotel()
         {
-            Hotel lakeWood = new Hotel();
-            Hotel bridgeWood = new Hotel();
-            Hotel ridgeWood = new Hotel();
-            lakeWood.HotelName = "Lakewood";
-            bridgeWood.HotelName = "Bridgewood";
-            ridgeWood.HotelName = "Ridgewood";
-            lakeWood.WeekdayRateForRegularCustomer = 110;
-            lakeWood.WeekendRateForRegularCustomer = 90;
-            bridgeWood.WeekdayRateForRegularCustomer = 160;
-            bridgeWood.WeekendRateForRegularCustomer = 60;
-            ridgeWood.WeekdayRateForRegularCustomer = 220;
-            ridgeWood.WeekendRateForRegularCustomer = 150;
             string expectedHotelName = "Lakewood";
-            int expectedWeekDayRateforRegularCustomer = 110;
-            int expectedWeekendRateforRegularCustomer = 90;
-            Assert.AreEqual(expectedHotelName, lakeWood.HotelName);
-            Assert.AreEqual(expectedWeekDayRateforRegularCustomer, lakeWood.WeekdayRateForRegularCustomer);
-            Assert.AreEqual(expectedWeekendRateforRegularCustomer, lakeWood.WeekendRateForRegularCustomer);
+            int expectedHotelRate = 220;
+            string actualHotelName = Operation.FindCheapestHotelName("10Sep2020", "11Sep2020");
+            int actualHotelRate = Operation.FindCheapestHotelRate("10Sep2020", "11Sep2020");
+            Assert.AreEqual(expectedHotelName, actualHotelName);
+            Assert.AreEqual(expectedHotelRate, actualHotelRate);
         }
     }
 }
