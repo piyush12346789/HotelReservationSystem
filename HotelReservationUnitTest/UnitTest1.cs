b54719113f4c9b2a27e056bca3728b8712065972
piyush12346789 <piyushgupta024@gmail.com>
2020-11-04 07:49:54 +0530

    [Piyush] Add: Method to add weekday and weekend rates for each hotel.

--- HotelReservationUnitTest/UnitTest1.cs
+++ HotelReservationUnitTest/UnitTest1.cs
@@ -6,15 +6,27 @@ namespace HotelReservationUnitTest
     [TestClass]
     public class UnitTest1
     {
-        [TestMethod]
-        public void Given_DateRange_Should_Return_CheapestHotel()
+        public void Adding_Weekday_And_Weekend_RegularCustomerRates_And_Verifying()
         {
-            string expectedHotelName = "Lakewood";
-            int expectedHotelRate = 220;
-            string actualHotelName = Operation.FindCheapestHotelName("10Sep2020", "11Sep2020");
-            int actualHotelRate = Operation.FindCheapestHotelRate("10Sep2020", "11Sep2020");
-            Assert.AreEqual(expectedHotelName, actualHotelName);
-            Assert.AreEqual(expectedHotelRate, actualHotelRate);
+            Hotel lakeWood = new Hotel
+            {
+                WeekdayRateForRegularCustomer = 110,
+                WeekendRateForRegularCustomer = 90
+            };
+            Hotel bridgeWood = new Hotel
+            {
+                WeekdayRateForRegularCustomer = 150,
+                WeekendRateForRegularCustomer = 50
+            };
+            Hotel ridgeWood = new Hotel
+            {
+                WeekdayRateForRegularCustomer = 220,
+                WeekendRateForRegularCustomer = 150
+            };
+            int expectedWeekDayRateforRegularCustomer = 110;
+            int expectedWeekendRateforRegularCustomer = 90;
+            Assert.AreEqual(expectedWeekDayRateforRegularCustomer, lakeWood.WeekdayRateForRegularCustomer);
+            Assert.AreEqual(expectedWeekendRateforRegularCustomer, lakeWood.WeekendRateForRegularCustomer);
         }
     }
 }
